8067b344e03adc7c25e866c68a9c6409ac68f085
buraaksenturk <buraak.senturk@gmail.com>
2021-06-03 16:13:00 +0300

    first commit

---
+++ src/components/Todo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="row">
+    <div class="col-md-6 offset-md-3 border border-dark text-white d-flex flex-row justify-content-between align-items-center pt-2 pb-2 mt-2">
+      <span>{{ todo.text }}</span>
+      <button class="btn btn-danger" @click="$emit('deleteTodo', todo.id)">Sil</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["todo"]
+}
+</script>
+
+<style scoped>
+
+</style>
